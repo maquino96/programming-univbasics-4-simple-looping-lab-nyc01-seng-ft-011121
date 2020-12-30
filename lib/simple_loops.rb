25962ffac7c331dce5667b2c2fbe7c75270cc6f6
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-30 21:27:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(str)
+  5.times do
+    puts str
+  end
+end
+
+def loop_message_n_times (str, n)
+  n.times do
+    puts str
+  end
+end
+
+def output_array(array)
+  
+end
+
+def return_string_array (array)
+  
+end
+
