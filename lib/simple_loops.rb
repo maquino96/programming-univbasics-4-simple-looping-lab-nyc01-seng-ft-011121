c38367a9ba0120e2cd8eb034e44b662da9610e19
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-30 21:33:38 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(str)
+  5.times do
+    puts str
+  end
+end
+
+def loop_message_n_times (str, n)
+  n.times do
+    puts str
+  end
+end
+
+def output_array(array)
+  array.each{|i| puts i}
+end
+
+def return_string_array (array)
+  array.collect{|i| i.to_s} 
+end
+
